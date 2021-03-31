708af7671538de2401420de7222e3c35b3e674d7
Navas <gabrielnavas@email.com>
2021-03-30 21:45:21 -0300

    refactory employee to functional

--- backend/src/models/employee.spec.ts
+++ backend/src/models/employee.spec.ts
@@ -1,54 +1,89 @@
-import { Address, Employee, Name } from './employee'
+import { employeModelFactory } from './employee'
 
 describe('Address Model', () => {
   test('should throw if street name is small', () => {
-    const testThrow = () => new Address(
-      'str',
-      22,
-      'any_district'
-    )
+    const testThrow = () => employeModelFactory({
+      name: {
+        firstName: 'Gabriel',
+        lastName: 'Navas'
+      },
+      address: {
+        streetName: 'str',
+        streetNumber: 22,
+        district: 'Village snakes'
+      },
+      birthDate: new Date(2000, 1, 1)
+    })
     const error = new Error('street name length must be between 3 and 80 characters.')
     expect(testThrow).toThrow(error)
   })
 
   test('should throw if street name is large', () => {
     const eigthOneCharacteres = Array(81).fill('a').join('')
-    const testThrow = () => new Address(
-      eigthOneCharacteres,
-      22,
-      'any_district'
-    )
+    const testThrow = () => employeModelFactory({
+      name: {
+        firstName: 'Gabriel',
+        lastName: 'Navas'
+      },
+      address: {
+        streetName: eigthOneCharacteres,
+        streetNumber: 22,
+        district: 'Village snakes'
+      },
+      birthDate: new Date(2000, 1, 1)
+    })
     const error = new Error('street name length must be between 3 and 80 characters.')
     expect(testThrow).toThrow(error)
   })
 
   test('should throw if street number is negative', () => {
-    const testThrow = () => new Address(
-      'street Julius Sam',
-      -2,
-      'any_district'
-    )
+    const testThrow = () => employeModelFactory({
+      name: {
+        firstName: 'Gabriel',
+        lastName: 'Navas'
+      },
+      address: {
+        streetName: 'street one',
+        streetNumber: -5,
+        district: 'Village snakes'
+      },
+      birthDate: new Date(2000, 1, 1)
+    })
     const error = new Error('street number cannot be negative.')
     expect(testThrow).toThrow(error)
   })
 
   test('should throw if district is small', () => {
-    const testThrow = () => new Address(
-      'street Julius Sam',
-      22,
-      'a'
-    )
+    const testThrow = () => employeModelFactory({
+      name: {
+        firstName: 'Gabriel',
+        lastName: 'Navas'
+      },
+      address: {
+        streetName: 'street one',
+        streetNumber: 22,
+        district: 'v'
+      },
+      birthDate: new Date(2000, 1, 1)
+    })
     const error = new Error('district length must be between 3 and 80 characters.')
     expect(testThrow).toThrow(error)
   })
 
   test('should throw if district is large', () => {
     const eigthOneCharacteres = Array(81).fill('a').join('')
-    const testThrow = () => new Address(
-      'street Julius Sam',
-      22,
-      eigthOneCharacteres
-    )
+    const testThrow = () => employeModelFactory({
+      name: {
+        firstName: 'Gabriel',
+        lastName: 'Navas'
+      },
+      address: {
+        streetName: 'street one',
+        streetNumber: 22,
+        district: eigthOneCharacteres
+      },
+      birthDate: new Date(2000, 1, 1)
+    })
     const error = new Error('district length must be between 3 and 80 characters.')
     expect(testThrow).toThrow(error)
   })
@@ -56,34 +91,36 @@ describe('Address Model', () => {
 
 describe('Name Model', () => {
   test('should throw if first name is small', () => {
-    const testThrow = () => new Name(
-      'a',
-      'any_last_name'
-    )
+    const testThrow = () => employeModelFactory({
+      name: {
+        firstName: 'G',
+        lastName: 'Navas'
+      },
+      address: {
+        streetName: 'street one',
+        streetNumber: 22,
+        district: 'Village Wolf'
+      },
+      birthDate: new Date(2000, 1, 1)
+    })
     const error = new Error('first name length must be between 2 and 30 characters.')
     expect(testThrow).toThrow(error)
   })
 
   test('should throw if last name is small', () => {
-    const testThrow = () => new Name(
-      'any_first_name',
-      'a'
-    )
+    const testThrow = () => employeModelFactory({
+      name: {
+        firstName: 'Gabriel',
+        lastName: 'n'
+      },
+      address: {
+        streetName: 'street one',
+        streetNumber: 22,
+        district: 'Village Wolf'
+      },
+      birthDate: new Date(2000, 1, 1)
+    })
     const error = new Error('last name length must be between 2 and 30 characters.')
     expect(testThrow).toThrow(error)
-  })
-})
-
-describe('Employee Model', () => {
-  test('should throw if birth date is more 120 years', () => {
-    const name = new Name('Gabriel', 'Navas')
-    const address = new Address('Street One', 22, 'Ville people')
-    const yearOld = 1900
-    const monthJanuary = 1
-    const dayOne = 1
-    const testThrow = () => new Employee(
-      name, address, new Date(yearOld, monthJanuary, dayOne)
-    )
-    expect(testThrow).toThrow()
   })
 })
--- backend/src/models/employee.ts
+++ backend/src/models/employee.ts
@@ -1,50 +1,57 @@
-export class Name {
-  constructor (
-    public readonly firstName: string,
-    public readonly lastName: string
-  ) {
-    if (this.firstName.length < 2 || this.firstName.length > 30) {
-      throw new Error('first name length must be between 2 and 30 characters.')
-    }
-    if (this.lastName.length < 2 || this.lastName.length > 20) {
-      throw new Error('last name length must be between 2 and 30 characters.')
-    }
+type Name = {
+  firstName: Readonly<string>
+  lastName: Readonly<string>
+}
+
+type Address = {
+  streetName: Readonly<string>
+  streetNumber: Readonly<number>
+  district: Readonly<string>
+}
+
+type Employee = {
+  name: Readonly<Name>,
+  address: Readonly<Address>,
+  birthDate: Readonly<Date>
+}
+
+const verifyName = (name: Name) => {
+  if (name.firstName.length < 2 || name.firstName.length > 30) {
+    throw new Error('first name length must be between 2 and 30 characters.')
+  }
+  if (name.lastName.length < 2 || name.lastName.length > 20) {
+    throw new Error('last name length must be between 2 and 30 characters.')
   }
 }
 
-export class Address {
-  constructor (
-    public readonly streetName: string,
-    public readonly streetNumber: number,
-    public readonly district: string
-
-  ) {
-    if (this.streetName.length < 4 || this.streetName.length > 80) {
-      throw new Error('street name length must be between 3 and 80 characters.')
-    }
-    if (this.streetNumber < 0) {
-      throw new Error('street number cannot be negative.')
-    }
-    if (this.streetNumber > Number.MAX_SAFE_INTEGER) {
-      throw new Error('very large street number.')
-    }
-    if (this.district.length < 2 || this.district.length > 80) {
-      throw new Error('district length must be between 3 and 80 characters.')
-    }
+const verifyAddress = (address:Address) => {
+  if (address.streetName.length < 4 || address.streetName.length > 80) {
+    throw new Error('street name length must be between 3 and 80 characters.')
+  }
+  if (address.streetNumber < 0) {
+    throw new Error('street number cannot be negative.')
+  }
+  if (address.streetNumber > Number.MAX_SAFE_INTEGER) {
+    throw new Error('very large street number.')
+  }
+  if (address.district.length < 2 || address.district.length > 80) {
+    throw new Error('district length must be between 3 and 80 characters.')
   }
 }
 
-export class Employee {
-  constructor (
-    public readonly name: Name,
-    public readonly address: Address,
-    public readonly birthDate: Date
-  ) {
-    const birthYear = this.birthDate.getUTCFullYear()
-    const nowYear = 2021
-    const differenceYear = nowYear - birthYear
-    if (differenceYear > 120) {
-      throw new Error('Employee cannot be more than 120 years old.')
-    }
+const verifyEmployee = (params: Employee) => {
+  verifyName(params.name)
+  verifyAddress(params.address)
+  const birthYear = params.birthDate.getUTCFullYear()
+  const nowYear = 2021
+  const differenceYear = nowYear - birthYear
+  if (differenceYear > 120) {
+    throw new Error('Employee cannot be more than 120 years old.')
   }
+}
+
+export const employeModelFactory = (params: Employee) => {
+  verifyEmployee(params)
+  const employee = params
+  return employee
 }
